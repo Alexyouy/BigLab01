7fde26b7e7ebc4f7b80026a10c172170fc89edeb
Fayou Zhu <s299724@studenti.polito.it>
2022-05-06 03:20:57 +0200

    fix bugs

--- react-film/src/components/FilmsLibrary.js
+++ react-film/src/components/FilmsLibrary.js
@@ -3,24 +3,21 @@ const starEmpty = '<svg id="empty-star" xmlns="http://www.w3.org/2000/svg" width
 const starFilled = '<svg id="filled-star" xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-star-fill" viewBox="0 0 16 16"><path d="M3.612 15.443c-.386.198-.824-.149-.746-.592l.83-4.73L.173 6.765c-.329-.314-.158-.888.283-.95l4.898-.696L7.538.792c.197-.39.73-.39.927 0l2.184 4.327 4.898.696c.441.062.612.636.282.95l-3.522 3.356.83 4.73c.078.443-.36.79-.746.592L8 13.187l-4.389 2.256z"/></svg> ';
 
 
-function Film(id, title, isFavorite = false, dateWatched, rating) {
+function Film(id, title, isFavorite = false, dateWatched, rating, star) {
     this.id = id;
     this.title = title;
     this.isFavorite = isFavorite;
     this.watchDate = dateWatched === undefined ? undefined : dayjs(dateWatched).format('YYYY-MM-DD');
     this.rating = rating;
     //Star icons
+    if(star==null){
     this.star = '';
     for (let i = 0; i < 5; i++) {
-        const starNow = (i < this.rating) ? starFilled : starEmpty;
+        let starNow = (i < this.rating) ? starFilled : starEmpty;
         this.star += starNow;
-    }
-    this.updateStar = () => {
-        this.star = '';
-        for (let i = 0; i < 5; i++) {
-            const starNow = (i < this.rating) ? starFilled : starEmpty;
-            this.star += starNow;
-        }
+    }}
+    else{
+        this.star=star;
     }
    
 }
@@ -28,12 +25,6 @@ function Film(id, title, isFavorite = false, dateWatched, rating) {
 function FilmLibrary() {
 
     this.list = [];
-
-    this.addNewFilmByRow = (id, title, isFavorite, dateWatched, rating) => {
-        const newFilm = new Film(id, title, isFavorite, dateWatched, rating);
-        this.list.push(newFilm);
-        return this.list;
-    }
 
     this.addNewFilm = ([...films]) => {
         films.forEach(f => {
@@ -43,20 +34,6 @@ function FilmLibrary() {
 
     this.getFilms = () => {
         return this.list;
-    }
-
-    this.updateFilm = (id, title, isFav, date, rating) => {
-        return this.list.filter(f => {
-            if (f.id === id) {
-                f.title = title;
-                f.isFavorite = isFav;
-                f.watchDate = dayjs(date).format('YYYY-MM-DD');
-                f.rating = rating;
-                return f;
-            } else {
-                return f;
-            }
-        });
     }
 }
 
